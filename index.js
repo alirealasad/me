62cb9f3067332d0afa29c2967a1c6042e75c7c60
Asad Ali <alidjasad@gmail.com>
2020-09-01 10:41:13 +0530

    realixe

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const ilovepdfSDK = require('ilovepdf-sdk');
 
 // function imagesHaveLoaded() {
 //     return Array.from(document.images).every((i) => i.complete);
@@ -6,6 +7,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch();
+    console.log("launching puppeteer...");
     const page = await browser.newPage();
     // const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
     // 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
@@ -14,6 +16,18 @@ const puppeteer = require('puppeteer');
     await page.goto('https://r-world.netlify.app/resume/cool',{waitUntil: 'networkidle0'});
     // await page.waitForFunction(imagesHaveLoaded, { timeout: 5000 });
     await page.pdf({path: 'resume.pdf', format: 'A4'});
+    console.log("Finished exporting");
+
+    if (process.env.PUBLIC_KEY && process.env.SECRET_KEY ) {
+      const sdk = new ilovepdfSDK(process.env.PUBLIC_KEY, process.env.SECRET_KEY);
+      const task = await sdk.createTask('compress');
+      console.log('compressing ...');
+      await task.addFile("./resume.pdf");
+      await task.process();
+      console.log('Finished compressing.');
+      await task.download("./resume.pdf");
+      console.log('Finished downloading.');
+    }
 
   await browser.close();
 })();
